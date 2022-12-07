9fac706e4a4312b5d0b85915780d7da82897735e
Alvaro <ixml3d@gmail.com>
2022-12-07 10:59:35 -0500

    users

--- portafolio/users/views.py
+++ portafolio/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from .forms import NewUserForm
 
 # Create your views here.
+class RegisterView(CreateView):
+	template_name = "registration/register.html"
+	form_class = NewUserForm
+
+	def form_valid(self, form):
+		form.save()
+		return redirect('login')
