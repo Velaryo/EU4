612d2785c5b174acada028c82af44c2cb0afc362
Alvaro <ixml3d@gmail.com>
2022-12-09 00:32:48 -0500

    staticsfiles 3

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -46,6 +46,7 @@ CSRF_TRUSTED_ORIGINS = ['https://alvaro-huarca.up.railway.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
